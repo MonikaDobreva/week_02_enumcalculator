0e8d2c7f3e05fc995445d1e185889ef0a21fa434
MonikaDobreva <m.dobreva@student.fontys.nl>
2022-03-08 12:15:52 +0100

    operator impl and tests

--- assignment/src/main/java/enumcalculator/Operator.java
+++ assignment/src/main/java/enumcalculator/Operator.java
@@ -13,8 +13,13 @@ import java.util.stream.Stream;
  * @author Pieter van den Hombergh / Richard van den Ham
  */
 public enum Operator {
-    //TODO Define enum values
-    ;
+
+    ADD("+", Integer::sum),
+    SUBTRACT("-", (a, b) -> a - b),
+    MULTIPLY("*", (a, b) -> a * b),
+    DIVIDE("/", (a, b) -> (a / b)),
+    POWER("**", (a, b) -> (int) Math.pow(a, b));
+
 
     /**
      * Get the operator using its symbol. This method does a linear search
@@ -24,8 +29,12 @@ public enum Operator {
      * @return operation when found, null otherwise.
      */
     public static Operator get(String symbol) {
-        //TODO Implement get method
-          throw new UnsupportedOperationException("method not implemented");
+        for (Operator operator : values()) {
+            if (operator.get().equals(symbol)) {
+                return operator;
+            }
+        }
+        return null;
     }
 
     /**
@@ -57,8 +66,13 @@ public enum Operator {
      * @return result of the computation.
      */
     public int compute(int a, int b) {
-        //TODO Implement compute method
-        return 0;
+        if(this.symbol == "/"){
+            if(b == 0){
+                throw new IllegalArgumentException("Cannot divide by 0!");
+            }
+        }
+        int comp = computation.applyAsInt(a, b);
+        return comp;
     }
 
     /**
@@ -67,7 +81,11 @@ public enum Operator {
      * @return String array containing all supported operations.
      */
     public static String[] supportedSymbols() {
-        //TODO Implement method
-        return null;
+        String[] symbols = {"+", "-", "*", "/", "**"};
+        return symbols;
+    }
+
+    public String get(){
+        return this.symbol;
     }
 }
--- assignment/src/test/java/enumcalculator/OperatorTest.java
+++ assignment/src/test/java/enumcalculator/OperatorTest.java
@@ -4,10 +4,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.fail;
 import org.assertj.core.api.Assumptions;
+import org.assertj.core.api.ThrowableAssert;
 import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import java.util.Arrays;
 
 /**
  * Parameterized test for enum operation. Look in [Kaczanowski, ch 3] on how to
@@ -33,23 +38,40 @@ public class OperatorTest {
     @ParameterizedTest
     @CsvSource( {
         // message, symbol, expected result, a,b
-        "add      , + ," + ( 2 + 3 ) + ", 2, 3 " //comma separated string
-        //TODO Add additional test cases here.
+            "add      , + ," + ( 2 + 3 ) + ", 2, 3 ",
+            "subtract , - ," + (2 - 3) + ", 2, 3 ",
+            "multiply , * ," + (2 * 3) + ", 2, 3 ",
+            "divide , / , " + (2 / 3) + ", 2, 3 ",
+            "power , ** , " + (2 * 4) + ", 2, 3 "
+
     } )
     public void testOperator( String message, String symbol, int expected, int a, int b ) {
         // use all inputs in the assertThat
-        //TODO Write assertThat statement in parameterized test method
-        fail( "test not yet implemented" );
+        assertThat(Operator.get(symbol).compute(a, b))
+                .as(message).isEqualTo(expected);
+        //fail( "test not yet implemented" );
     }
     
     /**
      * testSupportedOperators.
      * Should be +, -, *, /, **
      */
-    @Test
-    public void testSupportedOperators() {
-        //TODO Write assertThat statement to test whether all operations are supported
-        fail( "test not yet implemented" );
+    @ParameterizedTest
+    @CsvSource( {
+            "+",
+            "-",
+            "*",
+            "/",
+            "**"
+
+    } )
+    public void testSupportedOperators(String symbol) {
+        boolean contains = Arrays.asList(Operator.supportedSymbols()).contains(symbol);
+        boolean t = true;
+        assertThat(contains)
+                .isEqualTo(t);
+
+        //fail( "test not yet implemented" );
     }
     
     /**
@@ -57,7 +79,14 @@ public class OperatorTest {
      */
     @Test
     public void testDivisionByZeroNotAllowed() {
-        //TODO Write test to assure that a division by zero results in IllegalArgumentException
-        fail( "test not yet implemented" );
+        ThrowableAssert.ThrowingCallable code = () -> {
+            int a = 1;
+            int b = 0;
+            //Operator operator = Operator
+            Operator.get("/").compute(a, b);
+        };
+        assertThatCode(code).as("Division by zero not possible!")
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+        //fail( "test not yet implemented" );
     }
 }
